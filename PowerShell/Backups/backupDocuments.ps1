bef83589f82275b06a7add1a1f80834cb50f5a6b
Snickasaurus <Snickasaurus@users.noreply.github.com>
2018-03-27 07:12:37 -0400

    Create backupDocuments.ps1

---
+++ PowerShell/Backups/backupDocuments.ps1
@@ -0,0 +1,27 @@
+# Script Name   =   backupDocuments.ps1
+# Author        =   Snickasaurus
+# Date          =   20170201
+# Purpose       =   Use 7zip to archive "Documents" directory
+# Usage         =   ./backup-Documents.ps1
+# Requirements  =   7zip, backup location
+
+# Variables
+$TheTargetDirectory = "C:\Users\Me\Documents"                               # Archive target
+$TheBackupLocation = "\\freenas.local\Backups\Windows\GAMING\Documents\"    # Archive location
+$RententionPolicy = "20"                                                    # Number of Retention Units back to delete
+$RetentionUnit = "days"                                                     # This can be set to months/weeks/days/hours
+$CurrentDateTime = Get-Date -Format "yyyyMMdd-HHmmss"                       # Create a YearMonthDay-HourMinuteSecond time stamp
+$NewFileName = "Documents-"+$CurrentDateTime+".tar"                         # Set the archive name
+$Logging = $TheBackupLocation + $NewFileName                                # Set the logging directory
+
+# Function
+function Create-7zip([String] $aDirectory, [String] $aZipfile){
+    [string]$pathToZipExe = "C:\Program Files\7-zip\7z.exe";
+    [Array]$arguments = "a", "-ttar", "$aZipfile", "$aDirectory", "-mx9";
+    & $pathToZipExe $arguments;}
+
+# Create 7z archive of the game data directory
+Create-7zip $TheTargetDirectory $Logging
+
+# Check data retention policy
+dir $TheBackupLocation | where { ((get-date)-$_.LastWriteTime).$RetentionUnit -ge $RententionPolicy} | Remove-Item -Force
